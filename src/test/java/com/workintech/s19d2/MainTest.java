a5be9c58274b9b042b80517ad6cd2f4bbc5b99e1
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-18 15:13:25 +0300

    15 tests passed - project done

--- src/test/java/com/workintech/s19d2/MainTest.java
+++ src/test/java/com/workintech/s19d2/MainTest.java
@@ -37,7 +37,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
+/// /
 @DataJpaTest
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
